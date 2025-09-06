593f25d54192c94d72fb48dc103428e9da514222
Sabir Ali <alisabir167167@gmail.com>
2025-09-06 10:31:26 +0530

    feat(user): add user authentication routes and server integration
    Add register and login endpoints to handle user authentication

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import emailRoutes from "./routes/emailRoutes";
+import userRoutes from "./routes/userRoutes";
 
 dotenv.config();
 const app = express();
@@ -12,6 +13,8 @@ mongoose.connect(process.env.MONGO_URI!)
   .catch((err) => console.error(err));
 
 app.use("/api/email", emailRoutes);
+app.use("/api/user",userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
