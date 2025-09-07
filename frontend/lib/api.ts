92da3f5666e06810ddb586c89db7fbc992719880
Sabir Ali <alisabir167167@gmail.com>
2025-09-07 18:30:02 +0530

    fixing errrorrrr

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,22 +1,14 @@
 import axios from "axios";
 
-let BASE_URL = "https://zohomailassignment.onrender.com/api";
-
-if (typeof window !== "undefined" && window.location.hostname === "localhost") {
-  BASE_URL = "http://localhost:5000/api";
-}
-
 const API = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "http://localhost:5000/api", 
   withCredentials: true,
 });
 
 API.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const token = localStorage.getItem("token");
-    if (token && config.headers) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
